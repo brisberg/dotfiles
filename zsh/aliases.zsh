a9cb0a100ac70de0fde0b66c88a8aa8c4c5d1cda
Brandon Risberg <brandon.risberg@gmail.com>
2022-08-01 12:35:48 -0700

    Extracts base aliases into a aliases.zsh file

---
+++ zsh/aliases.zsh
@@ -0,0 +1,21 @@
+# Sourced from .zshrc
+# Place all zsh aliases in this file
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+alias zrc="subl ~/.zshrc"
+alias szrc="source ~/.zshrc"
+
+# Git aliases
+alias gs="git status"
+alias gci="git commit"
+alias gcim="git commit -m"
+alias gaa="git add ."
+alias gap="git add -p"
+alias grbc="git rebase --continue"
+alias grba="git rebase --abort"
+alias gl="git log --all --graph --decorate --oneline --simplify-by-decoration"
